3592054e01c48e16ec6b0ff66c66e1f017305d1c
Kartik_Sama <sai.karthik@iiitb.org>
2020-08-14 10:59:41 +0530

    Divison functionality added

--- assignment1.py
+++ assignment1.py
@@ -4,13 +4,18 @@ def addition(a,b):
 def subtraction(a,b):
 	return a-b;
 
+def division(a,b):
+        try:
+                return a/b;
+        except ZeroDivisionError:
+                return "Can't divide by zero";
 
 if __name__ == "__main__":
 	a = 3
 	b = 5
 	print(addition(a,b))
 	print(subtraction(a,b))
-
+        print(division(a,b))
 
 '''
 clone from github
